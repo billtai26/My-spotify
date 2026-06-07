ef3c8d6901d03f51b553a9cce7dd39ce16641693
anhtai26 <anhtaipxuan@gmail.com>
2026-06-07 10:18:46 +0700

    Feat: Create play song functionality & Queue

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -6,6 +6,7 @@ import {
 import { Outlet } from "react-router-dom"
 import LeftSidebar from "./components/LeftSidebar.tsx"
 import FriendsActivity from "./components/FriendsActivity.tsx"
+import AudioPlayer from "./components/AudioPlayer.tsx"
 
 const MainLayout = () => {
   const isMobile = false
@@ -16,6 +17,7 @@ const MainLayout = () => {
         direction="horizontal"
         className="flex-1 flex h-full overflow-hidden p-2"
       >
+        <AudioPlayer />
         {/* Left sidebar */}
         <ResizablePanel
           defaultSize={20}
