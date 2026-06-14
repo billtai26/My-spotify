78b195490b5d6d1202942e39ec6e61dd737e503b
Phan Xuân Anh Tài <anhtaipxuan@gmail.com>
2026-06-14 10:38:42 +0700

    Add Google logo to SignInOAuthButton

--- src/components/SignInOAuthButton.tsx
+++ src/components/SignInOAuthButton.tsx
@@ -18,6 +18,7 @@ const SignInOAuthButton = () => {
       onClick={signInWithGoogle}
       className="bg-white text-black px-4 py-2 rounded-md font-medium whitespace-nowrap"
     >
+      <img src="/google.png" alt="Google" className="size-5" />
       Continue with Google
     </button>
   )
